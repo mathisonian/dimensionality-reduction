68cd8a5779d493069917a179f8ae49df4ca1da42
Fred Hohman <fred.hohman@gmail.com>
2018-07-10 21:05:42 -0700

    move references to giant string, hacky

---
+++ components/bib.js
@@ -0,0 +1,41 @@
+let bibliography = "@article{scikit-learn,\
+ title={Scikit-learn: Machine Learning in {P}ython},\
+ author={Pedregosa, F. and Varoquaux, G. and Gramfort, A. and Michel, V.\
+         and Thirion, B. and Grisel, O. and Blondel, M. and Prettenhofer, P.\
+         and Weiss, R. and Dubourg, V. and Vanderplas, J. and Passos, A. and\
+         Cournapeau, D. and Brucher, M. and Perrot, M. and Duchesnay, E.},\
+ journal={Journal of Machine Learning Research},\
+ volume={12},\
+ pages={2825--2830},\
+ year={2011}\
+}\
+@inproceedings{sklearn_api,\
+  author    = {Lars Buitinck and Gilles Louppe and Mathieu Blondel and\
+               Fabian Pedregosa and Andreas Mueller and Olivier Grisel and\
+               Vlad Niculae and Peter Prettenhofer and Alexandre Gramfort\
+               and Jaques Grobler and Robert Layton and Jake VanderPlas and\
+               Arnaud Joly and Brian Holt and Ga{\"{e}}l Varoquaux},\
+  title     = {{API} design for machine learning software: experiences from the scikit-learn\
+               project},\
+  booktitle = {ECML PKDD Workshop: Languages for Data Mining and Machine Learning},\
+  year      = {2013},\
+  pages = {108--122},\
+}\
+@article{mcinnes2018umap,\
+     author = {{McInnes}, L. and {Healy}, J.},\
+     title = {UMAP: Uniform Manifold Approximation and Projection for Dimension Reduction},\
+     journal = {arXiv},\
+     keywords = {Statistics - Machine Learning, Computer Science - Computational Geometry, Computer Science - Learning},\
+     year = 2018,\
+}\
+@article{maaten2008visualizing,\
+  title={Visualizing data using t-SNE},\
+  author={Maaten, Laurens van der and Hinton, Geoffrey},\
+  journal={Journal of machine learning research},\
+  volume={9},\
+  number={Nov},\
+  pages={2579--2605},\
+  year={2008}\
+}";
+
+export default bibliography;
--- components/references.js
+++ components/references.js
@@ -1,45 +1,11 @@
 const React = require('react');
 const parse = require('bibtex-parser')
+import bibliography from './bib.js'
 
 const citationCache = {};
 const cite = (label) => {
   return citationCache[label.toUpperCase()];
 }
-
-// need to load in .bib file directly as a string
-let bibliography = "@inproceedings{test1,\
- author = {Lysenko, Mikola and Nelaturi, Saigopal and Shapiro, Vadim},\
- title = {Group morphology with convolution algebras},\
- booktitle = {Proceedings of the 14th ACM Symposium on Solid and Physical Modeling},\
- series = {SPM '10},\
- year = {2010},\
- isbn = {978-1-60558-984-8},\
- location = {Haifa, Israel},\
- pages = {11--22},\
- numpages = {12},\
- url = {http://doi.acm.org/10.1145/1839778.1839781},\
- doi = {10.1145/1839778.1839781},\
- acmid = {1839781},\
- publisher = {ACM},\
- address = {New York, NY, USA},\
-}\
-@inproceedings{test2, \
-  author = { Lysenko, Mikola and Nelaturi, Saigopal and Shapiro, Vadim}, \
-  title = { Group morphology with convolution algebras }, \
-  booktitle = { Proceedings of the 14th ACM Symposium on Solid and Physical Modeling }, \
-  series = {\
-    SPM '10},\
- year = {2010},\
- isbn = {978-1-60558-984-8},\
- location = {Haifa, Israel},\
- pages = {11--22},\
- numpages = {12},\
- url = {http://doi.acm.org/10.1145/1839778.1839781},\
- doi = {10.1145/1839778.1839781},\
- acmid = {1839781},\
- publisher = {ACM},\
- address = {New York, NY, USA},\
-}";
 
 const parsedBib = parse(bibliography);
 
